81289c2e559e4f34df72ae88e657886aa59251cf
srinathyvtalent <srinathy.vtalent@gmail.com>
2019-12-19 17:42:53 +0530

    operation class added in BAL

---
+++ TestProject/TestProject/BussinessAccessLayer/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BussinessAccessLayer
+{
+    public class Operation
+    {
+        public int Addition()
+        {
+            int a = 10;
+            int b = 20;
+            int c;
+            c = a + b;
+            return c;
+        }
+
+    }
+}
